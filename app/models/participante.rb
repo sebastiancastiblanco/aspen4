bba77eed2d7de8ff050178eac7a8c14ef2cb406e
Sebastian Castiblanco <sebastiancasti@gmail.com>
2013-09-06 16:00:48 -0500

    particoantes modelo

--- app/models/participante.rb
+++ app/models/participante.rb
@@ -4,16 +4,14 @@ class Participante < ActiveRecord::Base
   #Validaciones campos obligatorios
   validates :apellido, :nombre, :correo, :presence => { :message => "Campo obligatorio"}
   #Validaciones numericas
-  validates :documento,:telefono,:celular , numericality: { :only_integer => true,  :message => "Debe ser un valor Numerico" }
+  validates :documento, numericality: { :only_integer => true,  :message => "Debe ser un valor Numerico" }, :unless => "documento.nil?"
+  validates :telefono, numericality: { :only_integer => true,  :message => "Debe ser un valor Numerico" }, :unless => "telefono.nil?" 
+  validates :celular, numericality: { :only_integer => true,  :message => "Debe ser un valor Numerico" }, :unless => "celular.nil?" 
   #Validaciones de longitud
-  validates :apellido,:nombre, length: { minimum: 2,  :message => "Longitud minima de 2 caracteres"  }
-  validates :documento, length: { minimum: 4,  :message => "Longitud minima de 4 caracteres"  }
-  validates :telefono, length: { minimum: 7,  :message => "Longitud minima de 7 digitos"  }
-  validates :celular, length: { minimum: 10,  :message => "Longitud minima de 10 digitos"  }
-  
-
-  #validates :telefono, numericality: { :only_integer => true,  :message => "Debe ser un valor Numerico" }
-  #validates :documento, :numericality => { :only_integer => true , :message => "Valor Numerico" }
+  validates :apellido,:nombre, length: { minimum: 3,  :message => "Longitud minima de 3 caracteres"  }
+  validates :documento, length: { minimum: 4,  :message => "Longitud minima de 4 digitos"  }, :unless => "documento.nil?" 
+  validates :telefono, length: { minimum: 7,  :message => "Longitud minima de 7 digitos"  }, :unless => "telefono.nil?" 
+  validates :celular, length: { minimum: 10,  :message => "Longitud minima de 10 digitos"  }, :unless => "celular.nil?" 
 
   #Relacion muchos a muchos, para los roles de los participantes
   belongs_to :rol_participante
