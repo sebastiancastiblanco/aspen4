b2545cecc49a8804c883508306ebb972683c7e94
Sebastian Castiblanco <sebastiancasti@gmail.com>
2013-12-09 07:29:07 -0500

    version oficina asegurate

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,10 @@ module AspenVersion3
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es]
+
   end
 end
