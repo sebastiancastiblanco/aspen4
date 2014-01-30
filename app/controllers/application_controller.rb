1a8cbd8873da9e7b3db1bae4cb8dd29bb3ba1923
sebastian castiblanco <sebastiancasti@gmail.com>
2014-01-29 21:50:42 -0500

    sorcery login ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-	#private
-	#def current_user
-  	#	@current_user ||= User.find_by_auth_token!(cookies[:authToken]) if cookies[:authToken]
-	#end
+
 
   private
   def not_authenticated
