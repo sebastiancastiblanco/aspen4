0801135bbd98a2e49c3da3c2108553d570ecce02
sebastian castiblanco <sebastiancasti@gmail.com>
2014-04-13 14:21:38 -0500

    postgres id

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -3,15 +3,15 @@ class ReportesController < ApplicationController
   	#Recuperar el ultimo proceso modificado del usuario y mostrar el reporte
   	#recuperar todos los procesos del usaurio para listarlos en un combo box.
   	@proceso = current_abogado.procesos.order('created_at DESC').last
-    @logs = Log.where(abogado_id: current_abogado.id).order('created_at DESC').group(current_abogado.id).limit(4)
+    @logs = Log.where(abogado_id: current_abogado.id).order('created_at DESC, abogado_id').limit(4)
     @procesos = current_abogado.procesos
 
     #ultimos movimientos de un proceso en especifico
     if (!@proceso.nil?)
-       @procesoLog = Log.where(proceso_id: @proceso.id ).order('updated_at DESC').group(@proceso.id).limit(3)
-      @alertasPendientes = @proceso.alertas.where("termina < ?", Time.now).order("updated_at DESC").group("id").limit(3);
-      @actividades = @proceso.actividads.activos.order('updated_at DESC').group("id").limit(3)
-      @documentos = @proceso.documents.activos.order('updated_at DESC').group("id").limit(3)
+       @procesoLog = Log.where(proceso_id: @proceso.id ).order('updated_at DESC,proceso_id').group(@proceso.id).limit(3)
+      @alertasPendientes = @proceso.alertas.where("termina < ?", Time.now).order("updated_at DESC,proceso_id").limit(3);
+      @actividades = @proceso.actividads.activos.order('updated_at DESC,proceso_id').limit(3)
+      @documentos = @proceso.documents.activos.order('updated_at DESC,proceso_id').limit(3)
    
     end
    
