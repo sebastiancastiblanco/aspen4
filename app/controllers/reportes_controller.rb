b62fbb214f744d045d41b0acb8f1e9183c0678de
sebastian castiblanco <sebastiancasti@gmail.com>
2014-04-13 14:44:46 -0500

    postgres id

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -2,16 +2,16 @@ class ReportesController < ApplicationController
   def index
   	#Recuperar el ultimo proceso modificado del usuario y mostrar el reporte
   	#recuperar todos los procesos del usaurio para listarlos en un combo box.
-  	@proceso = current_abogado.procesos.order('created_at DESC').last
-    @logs = Log.where(abogado_id: current_abogado.id).order('created_at DESC, abogado_id').limit(4)
+  	@proceso = current_abogado.procesos.order('id,created_at DESC').last
+    @logs = Log.where(abogado_id: current_abogado.id).order('id,created_at DESC').limit(4)
     @procesos = current_abogado.procesos
 
     #ultimos movimientos de un proceso en especifico
     if (!@proceso.nil?)
-       @procesoLog = Log.where(proceso_id: @proceso.id ).order('procesos.id, updated_at DESC').limit(5)
-      @alertasPendientes = @proceso.alertas.where("termina < ?", Time.now).order("procesos.id,updated_at DESC").limit(4);
-      @actividades = @proceso.actividads.activos.order('procesos.id,updated_at DESC').limit(2)
-      @documentos = @proceso.documents.activos.order('procesos.id,updated_at DESC').limit(1)
+       @procesoLog = Log.where(proceso_id: @proceso.id ).order('id, updated_at DESC').limit(5)
+      @alertasPendientes = @proceso.alertas.where("termina < ?", Time.now).order("id,updated_at DESC").limit(4);
+      @actividades = @proceso.actividads.activos.order('id,updated_at DESC').limit(2)
+      @documentos = @proceso.documents.activos.order('id,updated_at DESC').limit(1)
    
     end
    
@@ -25,9 +25,9 @@ class ReportesController < ApplicationController
    @proceso = Proceso.find(params[:proceso_id])
    #ultimos movimientos de un proceso en especifico
     @procesoLog = Log.where(proceso_id: @proceso.id ).order('updated_at DESC').group("id").limit(3)
-    @alertasPendientes = @proceso.alertas.where("termina < ?", Time.now).order("updated_at DESC").group("id").limit(3);
-    @actividades = @proceso.actividads.activos.order('updated_at DESC').group("id").limit(3)
-    @documentos = @proceso.documents.activos.order('updated_at DESC').group("id").limit(3)
+    @alertasPendientes = @proceso.alertas.where("termina < ?", Time.now).order("updated_at DESC").limit(3);
+    @actividades = @proceso.actividads.activos.order('updated_at DESC').limit(3)
+    @documentos = @proceso.documents.activos.order('updated_at DESC').limit(3)
 
    
     respond_to do |format|
