f01c682f151b62b884cdd3b2c8bffbdcaa296cdb
sebastian castiblanco <sebastiancasti@gmail.com>
2014-04-18 18:47:05 -0500

    datepicker heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,7 @@ AspenVersion3::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Expands the lines which load the assets
-  config.assets.debug = true
+ 
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
