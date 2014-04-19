95dcae7fd9d5f471a75ed59f219a81d2fbd49cce
sebastian castiblanco <sebastiancasti@gmail.com>
2014-04-19 16:25:01 -0500

    eventos heroku 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,10 @@
 class EventosController < ApplicationController
-  
-  #Recupara la lista de eventos del usuario.
+    #Recupara la lista de eventos del usuario.
   def index
-    @event = Evento.new
+    @evento = Evento.new
     #Recuperar los eventos del usuario logeado
-    #@eventos = current_user.eventos
-    @eventos = Evento.all
+    #@eventos = current_abogado.eventos
+    @eventos = current_abogado.eventos
     #respond de la funcion
     respond_to do |format| 
       format.html # index.html.erb 
@@ -15,29 +14,37 @@ class EventosController < ApplicationController
 
   # Crea la instancia del evento a crear en el la plantilla _form.html.erb
   def new
-    @event = Evento.new
+    @evento = Evento.new
     if (params[:date])
-        @event.start = params[:date]
-        @event.end = params[:date]
+        @evento.start = params[:date]
+        @evento.end = params[:date]
     end
   end
   
   #crea el evento en el calendario,  ejecuta el archivo create.js.erb
   #Asociandolo al usuario que lo creo.
   def create
-      @event = Evento.new(params[:event])
-      #setear la fecha en un solo campo
-      
-
-      respond_to do |format|
-        if @event.save
-          #Salvar relacion entre el evento y el usuario que lo creo
-          @event.usuario_eventos.create(:usuario_id => current_user.id,:propietario_id => current_user.id,:evento_id => @event.id)
+      @evento = Evento.new(params[:evento])
+     zone = ActiveSupport::TimeZone.new("America/Bogota")
+      if !@evento.start.nil?
         
-            format.json { render json: @event, status: :created, location: @event }
+      end
+      #if !@evento.end.nil?
+       #   
+      #end
+      
+      respond_to do |format|
+        if @evento.save
+          #Salvar relacion entre el evento y el usuario que lo creo
+          @evento.usuario_eventos.create(:abogado_id => current_abogado.id,:propietario_id => current_abogado.id,:evento_id => @evento.id)
+          
+          @evento.start.in_time_zone(zone)
+          @evento.end.in_time_zone(zone)
+
+            format.json { render json: @evento, status: :created, location: @evento }
             format.js
         else
-            format.json { render json: @event.errors, status: :unprocessable_entity }
+            format.json { render json: @evento.errors, status: :unprocessable_entity }
             format.js
         end
       end
@@ -46,19 +53,19 @@ class EventosController < ApplicationController
   
   #recupera la informacion del evento y ejcuta edit.js.erb en la plantilla _edit_form.html.erb
   def edit
-    @event = Evento.find_by_id(params[:id])
+    @evento = Evento.find_by_id(params[:id])
      
   end
   
   #actualiza los eventos y ejecuta update.js.erb
   def update
-      @event = Evento.find_by_id(params[:idEvent])
+      @evento = Evento.find_by_id(params[:id])
       respond_to do |format|
-       if @event.update_attributes(params[:event])
-           format.json { render json: @event, status: :created, location: @event }
+       if @evento.update_attributes(params[:evento])
+           format.json { render json: @evento, status: :created, location: @evento }
            format.js
        else
-           format.json { render json: @event.errors, status: :unprocessable_entity }
+           format.json { render json: @evento.errors, status: :unprocessable_entity }
            format.js
        end
       end
@@ -66,22 +73,29 @@ class EventosController < ApplicationController
 
   #Modifica la ubicacion de los eventos y ejecuta es JS de move.js.erb
   def move
-      @event = Evento.find(params[:id])
-    if @event
-      @event.start = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@event.start))
-      @event.end = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@event.end))
-      @event.allDay = params[:allDay]
-      @event.save
+      @evento = Evento.find(params[:id])
+    if @evento
+      @evento.start = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@evento.start))
+      @evento.end = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@evento.end))
+      @evento.allDay = params[:allDay]
+      @evento.save
+      #En caso de que el vento sea una alerta, se debe actulizar la fecha de incio de la alerta. 
+      if !@evento.alerta_id.nil?
+          @alerta = Alerta.find(@evento.alerta_id)
+          @alerta.termina = @evento.start
+          @alerta.save!
+      end
     end
   end
   
   #redimensaiona los eventos y ejecuta es JS de resize.js.erb
   def resize
-    @event = Evento.find(params[:id])
+    @evento = Evento.find(params[:id])
+
     respond_to do |format|
-      if @event
-          @event.endtime = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@event.endtime))
-          @event.save
+      if @evento
+          @evento.end = (params[:minute_delta].to_i).minutes.from_now((params[:day_delta].to_i).days.from_now(@evento.end))
+          @evento.save
       end    
       format.js
     end
@@ -89,8 +103,11 @@ class EventosController < ApplicationController
   
   #Elimina eventos y ejecuta es JS de destroy.js.erb
   def destroy
-    @event = Evento.find_by_id(params[:id])
-    @event.destroy
+    @evento = Evento.find_by_id(params[:id])
+    #desactivar la alerta desde el calendario.
+    @alerta = Alerta.find(@evento.alerta_id)
+    @alerta.update_attribute(:activo, false)
+    @evento.destroy
     respond_to do |format|
       format.html { redirect_to events_url }
       format.json { head :no_content }
@@ -98,17 +115,16 @@ class EventosController < ApplicationController
     end
 
   end
- end
+
 
 
   def get_events
-    #@events = Event.find(:all, :conditions => ["starttime >= '#{Time.at(params['start'].to_i).to_formatted_s(:db)}' and endtime <= '#{Time.at(params['end'].to_i).to_formatted_s(:db)}'"] )
-    @events = Evento.all 
+    #@eventos = Event.find(:all, :conditions => ["starttime >= '#{Time.at(params['start'].to_i).to_formatted_s(:db)}' and endtime <= '#{Time.at(params['end'].to_i).to_formatted_s(:db)}'"] )
+    @eventos = Evento.all 
     events = [] 
-    @events.each do |event|
+    @eventos.each do |event|
       events << {:id => event.id, :title => event.title, :description => event.description || "Some cool description here...", :start => "#{event.starttime.iso8601}", :end => "#{event.endtime.iso8601}", :allDay => event.all_day, :recurring => (event.event_series_id)? true: false}
     end
     render :text => events.to_json
   end
-  
-  
+end
