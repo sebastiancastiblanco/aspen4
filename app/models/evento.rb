740eb3d45e3399053170199553ab5857791a8fde
sebastian castiblanco <sebastiancasti@gmail.com>
2014-04-19 15:44:32 -0500

    eventos heroku

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -1,7 +1,7 @@
 class Evento < ActiveRecord::Base
   attr_accessible :allDay, :className, :color, :end, :start, :title, :url,:dsecription,:alerta_id,:event_id,:usuario_id,:abogado_id
   #validaciones campos obligatorios
-  #validates :title, :presence => { :message => "Campo obligatorio"}
+  validates :title, :presence => { :message => "Campo obligatorio"}
 
    has_many :usuario_eventos
    has_many :abogados, through: :usuario_eventos
